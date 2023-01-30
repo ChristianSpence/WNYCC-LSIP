08d2961aa03c88419f32991d74c3c027ec6a5e30
Christian Spence <mail@christianspence.co.uk>
2023-01-30 11:13:43 +0000

    Initial commit

---
+++ R/build-data.R
@@ -0,0 +1,159 @@
+# Defines all LAD codes for the projects
+geog_codes <- list(wy = paste0("E080000", 32:36),
+                   ny = c("E0600000014", # York
+                          paste0("E070001", 63:69) # Craven:Selby
+                   )
+)
+
+# Transforms academic years of format "202122" into "2021/22"
+academic_year <- function(x) {
+  return(paste(substr(x, 1, 4), substr(x, 5, 6), sep = "/"))
+}
+
+# Contains all the different dimensions that various datasets use and we need, to enable reshaping of only variables in datasets into a long format for Shiny app
+
+dimensions <- c("date",
+                "geography_name", "geography_code",
+                "school_type_group", "school_type",
+                "characteristic_type", "characteristic_value",
+                "school_type_group", "school_type",
+                "subject_area", "subject_name",
+                "subject_level", "age_youth_adult",
+                "age_detailed", "sex", "ethnicity_group",
+                "age_summary_with_unknowns",
+                "ssa_t1_desc",
+                "notional_nvq_level",
+                "apprenticeships_or_further_education",
+                "level_or_type", "age_group",
+                "start_age", "prior_attainment_group",
+                "employment_status", "lldd", "ethnicity",
+                "ssa_t1", "ssa_t2",
+                "institution_group", "institution_type",
+                "cohort_level_group", "cohort_level",
+                "characteristic_group", "characteristic",
+                "data_type")
+
+# Contains all the dimensions in datasets that we can discard
+dims_to_remove <- c("time_identifier", "geographic_level",
+                    "country_code", "country_name",
+                    "region_code", "region_name",
+                    "pcon_code", "pcon_name",
+                    "version",
+                    "old_la_code",
+                    "english_devolved_area_code", "english_devolved_area_name",
+                    "opportunity_area_code", "opportunity_area_name",
+                    "level_methodology")
+
+# KS4 ---------------------------------------------------------------------
+
+ks4 <- readr::read_csv("data/KS4/data/2122_lad_pr_data.csv") |>
+  dplyr::filter(lad_code %in% c(geog_codes$wy, geog_codes$ny)) |>
+  dplyr::select(-dplyr::any_of(dims_to_remove)) |>
+  dplyr::rename(date = time_period,
+                geography_code = lad_code,
+                geography_name = lad_name) |>
+  dplyr::mutate(date = academic_year(date)) |>
+  tidyr::pivot_longer(cols = -(dplyr::any_of(dimensions)),
+                      names_to = "variable_code")
+
+ks4_lookup <- readr::read_delim("data/KS4/data-guidance/data-guidance.txt",
+                                delim = "|", trim_ws = TRUE,
+                                skip = 26, n_max = 19)
+ks4_lookup <- ks4_lookup[-1, ]
+
+ks4 <- dplyr::inner_join(ks4, ks4_lookup, by = c("variable_code" = "Variable name")) |>
+  dplyr::rename(variable_name = `Variable description`)
+
+saveRDS(ks4, "data/app/ks4.rds")
+
+
+# A level -----------------------------------------------------------------
+
+a_level_lookup <- readr::read_delim("data/A-level/data-guidance/data-guidance.txt",
+                                    delim = "|", trim_ws = TRUE,
+                                    skip = 28) |>
+  dplyr::filter(!is.na(`Variable description`), !grepl("---", `Variable description`), `Variable description` != "Variable description")
+
+
+a_level <- lapply(list.files("data/A-level/data", full.names = TRUE),
+                  function(file) {
+                    readr::read_csv(file, col_types = readr::cols(.default = readr::col_character()),
+                                    na = c("c", "z")) |>
+                      dplyr::select(-dplyr::any_of(dims_to_remove)) |>
+                      dplyr::rename(date = time_period,
+                                    geography_code = new_la_code,
+                                    geography_name = la_name) |>
+                      dplyr::mutate(date = academic_year(date)) |>
+                      dplyr::filter(geography_code %in% c(geog_codes$wy,
+                                                       geog_codes$ny)) |>
+                      tidyr::pivot_longer(cols = -(dplyr::any_of(dimensions)),
+                                          names_to = "variable_code") |>
+                      dplyr::left_join(a_level_lookup,
+                                       by = c("variable_code" = "Variable name")) |>
+                      dplyr::rename(variable_name = `Variable description`)
+                  }) |>
+  setNames(basename(list.files("data/A-level/data")))
+
+saveRDS(a_level, "data/app/a-level.rds")
+
+
+# FE ----------------------------------------------------------------------
+
+fe_lookup <- readr::read_delim("data/FE/data-guidance/data-guidance.txt",
+                               delim = "|", trim_ws = TRUE,
+                               skip = 28) |>
+  dplyr::filter(!is.na(`Variable description`), !grepl("---", `Variable description`), `Variable description` != "Variable description")
+
+fe <- lapply(list.files("data/FE/data", full.names = TRUE),
+             function(file) {
+               if (!grepl("provider|geography-summary", file)) {
+                 print(file)
+                 readr::read_csv(file, na = c("z"), col_types = readr::cols(.default = readr::col_character())) |>
+                   dplyr::select(-dplyr::any_of(dims_to_remove)) |>
+                   dplyr::rename(date = time_period,
+                                 geography_code = lad_code,
+                                 geography_name = lad_name) |>
+                   dplyr::mutate(date = academic_year(date)) |>
+                   dplyr::filter(geography_code %in% c(geog_codes$wy,
+                                                       geog_codes$ny)) |>
+                   tidyr::pivot_longer(cols = -(dplyr::any_of(dimensions)),
+                                       names_to = "variable_code") |>
+                   dplyr::left_join(fe_lookup,
+                                    by = c("variable_code" = "Variable name")) |>
+                   dplyr::rename(variable_name = `Variable description`)
+               }
+
+             }) |>
+  setNames(basename(list.files("data/FE/data")))
+
+saveRDS(fe, "data/app/fe.rds")
+
+
+# FE destination ----------------------------------------------------------
+
+fe_dest_lookup <- readr::read_delim("data/FE_destination/data-guidance/data-guidance.txt",
+                                    delim = "|", trim_ws = TRUE,
+                                    skip = 52) |>
+  dplyr::filter(!is.na(`Variable description`), !grepl("---", `Variable description`), `Variable description` != "Variable description")
+
+fe_dest <- lapply(list.files("data/FE_destination/data", full.names = TRUE),
+                  function(file) {
+                    readr::read_csv(file, na = c("c", "x", "z", "low")) |>
+                      dplyr::select(-dplyr::any_of(dims_to_remove)) |>
+                      dplyr::select(-c(lad_code, lad_name)) |>
+                      dplyr::rename(date = time_period,
+                                    geography_code = new_la_code,
+                                    geography_name = la_name) |>
+                      dplyr::mutate(date = academic_year(date)) |>
+                      dplyr::filter(geography_code %in% c(geog_codes$wy,
+                                                          geog_codes$ny)) |>
+                      tidyr::pivot_longer(cols = -(dplyr::any_of(dimensions)),
+                                          names_to = "variable_code") |>
+                      dplyr::left_join(fe_dest_lookup,
+                                       by = c("variable_code" = "Variable name")) |>
+                      dplyr::rename(variable_name = `Variable description`)
+                  }) |>
+  setNames(basename(list.files("data/FE_destination/data")))
+
+saveRDS(fe_dest, "data/app/fe_dest.rds")
+
--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # WNYCC-LSIP
-West &amp; North Yorkshire Chamber of Commerce Local Skills Improvement Plans
+
+West & North Yorkshire Chamber of Commerce Local Skills Improvement Plans
+
+## Data
+
+Data for this project is collected from four main locations:
+
+- Department for Education Statistics (school and further education)
+- Higher Education Statistics Agency (universities)
+- Office for National Statistics (via NOMIS) (employment and business data)
+- The Data City (including Lightcast data) (individual company-level data and job advertisement/skills data)
+
+In the first phase, data is aggregated from the first three sources to allow the client to determine exactly which data variables are needed. Raw downloaded data from the sources us saved in `/data-raw`, processed via scripts stored in `/R`, with final datasets saved in `/data`. A Shiny web application that allows quick investigation of the data is provided in `/app`.
+
+Sources and data transformations for each of the four main locations is described here.
+
+### Department for Education
+
+DfE data is not directly queryable, instead selected from menus  [https://explore-education-statistics.service.gov.uk/find-statistics](here).
+
+Each downloaded .zip file contains a `data` folder and a `data-guidance` folder. The latter includes meta data and variable name lookups which are extracted in the scripts for each data source and joined to the `data` file.
+
+#### Key Stage 4
+
+- KS4 local authority data
+Original file: `data-raw/key-stage-4-performance-revised_2021-22.zip`
+Unzipped data: `data/KS4`
+Processed data (wide): `data/csv/ks4.csv`
+Processed data (long): `data/app/ks4.rds`
+
+#### A-level
+[https://explore-education-statistics.service.gov.uk/find-statistics/a-level-and-other-16-to-18-results](Source)
+
+Original file: `data-raw/a-level-and-other-16-to-18-results_2021-22.zip`
+Unzipped data: `data/A-level`
+Processed data (wide): `data/csv/a-level.csv`
+Processed data (long): `data/app/a-level.rds`
+
+#### FE
+
+[https://explore-education-statistics.service.gov.uk/find-statistics/further-education-and-skills](Source)
+
+Original file: `data-raw/further-education-and-skills_2021-22.zip`
+Unzipped file: `data/FE`
+Processed data (wide): `data/csv/a-level.csv`
+Processed data (long): `data/app/a-level.rds`
+
+#### FE Destination Measures:
+
+[https://explore-education-statistics.service.gov.uk/find-statistics/16-18-destination-measures](Source)
+
+Original file: `data-raw/16-18-destination-measures_2020-21.zip`
+Unzipped file: `data/FE_destination`
+Processed data (wide): `data/csv/fe_dest.csv`
+Processed data (long): `data/app/fe_dest.rds`
+
+### HESA
+
+HESA data is stored at persistent URLs.
+
