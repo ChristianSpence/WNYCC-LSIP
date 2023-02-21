e8eaa3fc9ec1ef469240598468ac084c85c7dc9e
Christian Spence <mail@christianspence.co.uk>
2023-02-21 14:59:39 +0000

    Update DC API to produce single file for each variable

--- R/datacity api.R
+++ R/datacity api.R
@@ -90,14 +90,35 @@ datacity_la_processed <- lapply(datacity_las, function(LA) {
 })
 
 # temporary serial dump of the above object to come back to
-saveRDS(datacity_la_processed, "data/datacity/datacity_la_processed.rds")
+# saveRDS(datacity_la_processed, "data/datacity/datacity_la_processed.rds")
+# datacity_la_processed <- readRDS("~/Projects/WNYCC-LSIP/data/datacity/datacity_la_processed.rds")
 
+filtered <- list()
+for (i in names(datacity_la_processed)) {
+  filtered[[i]] <- datacity_la_processed[[i]][grepl("AveragePostingDuration|JobPostingsBy|AverageAdvertisedSalary", names(datacity_la_processed[[i]]))]
+}
 
+grouped <- lapply(seq_along(filtered), function(area) {
+  tables <- names(filtered[[area]])
+  dfs <- list()
+  for (table in tables) {
+      dfs[[table]] <- dplyr::mutate(filtered[[area]][[table]], LA = names(filtered)[[area]], .before = 1)
+  }
+  return(dfs)
+}) |> setNames(names(filtered))
 
+out <- list()
 
+for (table in names(grouped[[1]])) {
+  for (area in names(grouped)) {
+    out[[table]] <- grouped[[area]][[table]]
+  }
+  out[[table]] <- dplyr::bind_rows(out[[table]], grouped[[area]][[table]])
+}
 
-
-
+lapply(seq_along(out), function(df) {
+  readr::write_csv(out[[df]], paste0("data/csv/datacity/", names(out)[df], ".csv"))
+})
 
 
 
