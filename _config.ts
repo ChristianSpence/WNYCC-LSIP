9b3f53a5a2efc05dad04da9118a1fa40850efc62
Giles Dring <giles@dringtech.com>
2023-02-27 13:22:00 +0000

    Adding some (BROKEN) interactivity to the KS4 site

--- _config.ts
+++ _config.ts
@@ -7,6 +7,7 @@ import minify_html from "lume/plugins/minify_html.ts";
 import postcss from "lume/plugins/postcss.ts";
 import oiCharts from "oi-lume-charts/mod.ts";
 import csvLoader from "oi-lume-utils/loaders/csv-loader.ts";
+import autoDependency from "oi-lume-utils/processors/auto-dependency.ts";
 
 const search = { returnPageData: true };
 const site = lume({
@@ -14,16 +15,17 @@ const site = lume({
   location: new URL('https://open-innovations.github.io/WNYCC-LSIP'),
 }, { search });
 
-site.use(base_path());
 site.use(date());
 site.use(inline());
 site.use(metas());
 site.use(postcss());
 site.use(oiCharts({
-  assetPath: "assets/oi",
+  assetPath: "/assets/oi",
   componentNamespace: "oi",
 }));
 
+site.process(['.html'], autoDependency);
+site.use(base_path());
 site.use(minify_html());
 
 site.loadData([".csv"], csvLoader);
