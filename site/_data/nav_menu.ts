fa37b7d1d18ba159e21250552f01d019fefd5d2f
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-03-20 17:10:02 +0000

    Update nav_menu.ts
    adding FE button

--- site/_data/nav_menu.ts
+++ site/_data/nav_menu.ts
@@ -4,11 +4,12 @@ export default [
     children: [
       { name: "Key Stage 4", link: "/supply/ks4" },
       { name: "A and AS Level", link: "/supply/a-level" },
-      { name: "16-18 Destinations", link:"/supply/fe_dest"}
-    ]
+      { name: "16-18 Destinations", link: "/supply/fe_dest" },
+      { name: "Further Education", link: "/supply/fe" },
+    ],
   },
   {
     name: "Demand",
-    link: "/demand"
-  }
-]
+    link: "/demand",
+  },
+];
