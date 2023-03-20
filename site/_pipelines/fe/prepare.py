18e3d6bf0e11a150387c6d4f26515853bdd817ff
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-03-20 17:11:07 +0000

    Add FE visualisations

--- site/_pipelines/fe/prepare.py
+++ site/_pipelines/fe/prepare.py
@@ -5,10 +5,9 @@ import re
 from lookups import LA_CODES
 from util import slugify
 
-def load_data(group=None):
+def load_data(filepath, group=None):
 
     #read csv
-    filepath = 'data/csv/fe/Basic skills - regional breakdown.csv'
     data = pd.read_csv(filepath)
 
     #filtering by CoC
@@ -30,10 +29,10 @@ if __name__ == '__main__':
     os.makedirs(OUTDIR, exist_ok=True)
 
     #load the data
-    data = load_data(group)
+    data = load_data('data/csv/fe/Basic skills - regional breakdown.csv', group)
 
     #temp data filter
-    #data = data[data.geography_code == 'E07000167']
+    #data = data[data.geography_code == 'E08000032']
     mycols = ['Basic Skills (Excluding digital skills)', 'Basic Skills (Including digital skills)','ESOL', 'Maths', 'English', 'Essential Digital Skills']
     
 
@@ -69,8 +68,29 @@ if __name__ == '__main__':
 
     stats=stats.round(2)
 
+    fe_and_skills = load_data('data/csv/fe/Further education and skills geography - detailed summary.csv', group)
+    fe_and_skills = fe_and_skills[fe_and_skills.age_group == 'Total'].drop(columns=['age_group'])
+    fe_and_skills = fe_and_skills[fe_and_skills.date == '2021/22']
+    fe_and_skills  = fe_and_skills[fe_and_skills.level_or_type.str.endswith('Total')].drop(columns=['new_la_code', 'date', 'la_name', 'level_or_type']).set_index('geography_code')
+
+    e_and_t = load_data('data/csv/fe/Education and training geography - local authority district.csv', group)
+    e_and_t = e_and_t.replace(to_replace=r'^lo.$', value=0, regex=True)
+    e_and_t = e_and_t[(e_and_t.date == '2021/22') & (e_and_t.sex == 'Total') & (e_and_t.ethnicity_group == 'Total')].drop(columns=[
+                                                                                'date', 'sex', 'ethnicity_group', 'notional_nvq_level']).set_index('ssa_t1_desc')
+    
+    for k in ['e_and_t_aims_enrolments', 'e_and_t_aims_ach']:
+         e_and_t[k] = pd.to_numeric(e_and_t[k])
+    e_and_t.drop(['Total'],inplace=True)
+    e_and_t = e_and_t.groupby('ssa_t1_desc').sum(numeric_only=True)
+
+    summary = stats.drop(columns='ap_ratio').sum()
+    #print(e_and_t)
+
     #write to file
     basic_skills.to_csv(os.path.join(OUTDIR, 'basic_skills.csv'))
+    fe_and_skills.to_csv(os.path.join(OUTDIR, 'fe_type.csv'))
+    e_and_t.sort_values(by=['e_and_t_aims_enrolments'], ascending=False).to_csv(os.path.join(OUTDIR, 'e_and_t_by_ssa_t1_desc.csv'))
     stats.to_csv(os.path.join(OUTDIR, 'stats.csv'))
+    summary.to_json(os.path.join(OUTDIR, 'summary.json'))
 
       
