b4271aff51fd1bb48d69ed0ee1e239591bc1f436
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-03-27 13:52:24 +0100

    re-order nav menu

--- site/_data/nav_menu.ts
+++ site/_data/nav_menu.ts
@@ -2,10 +2,10 @@ export default [
   {
     name: "Supply",
     children: [
+      { name: "Further Education", link: "/supply/fe" },
+      { name: "16-18 Destinations", link: "/supply/fe_dest" },
       { name: "Key Stage 4", link: "/supply/ks4" },
       { name: "A and AS Level", link: "/supply/a-level" },
-      { name: "16-18 Destinations", link: "/supply/fe_dest" },
-      { name: "Further Education", link: "/supply/fe" },
     ],
   },
   {
