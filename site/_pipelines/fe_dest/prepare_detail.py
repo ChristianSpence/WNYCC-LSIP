6d15df1e110574bb29bffc85d694a2ecd9a11b8e
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-03-28 13:45:50 +0100

    update prepare script

--- site/_pipelines/fe_dest/prepare_detail.py
+++ site/_pipelines/fe_dest/prepare_detail.py
@@ -8,16 +8,15 @@ from util import slugify, load_data, filtering, path_name, drop_totals
 if __name__ == '__main__':
 
     # specify filepath
-    filepath = 'data/csv/fe_dest/16-18LocalAuthorityLevelDestinations.csv'
+    filepath = 'data/csv/fe_dest/16-18 local authority level destinations.csv'
     group = sys.argv[1]
 
-    data = load_data(filepath, group=group, fill_na=True, na_values=['NA'], value=0)
+    data = load_data(filepath, group=group, fill_na=True, value=2)
+    SEN_data = data.drop(columns= ['institution_group', 'cohort_level', 'institution_type'])
     data = data[data.institution_group == 'State-funded mainstream schools & colleges'].drop(columns=
     ['institution_group', 'cohort_level', 'institution_type'])
     data = data[data.data_type != 'Percentage'].drop(columns=['data_type'])
 
-    data = drop_totals(data)
-    # , 'fe_level_2', 'fe_entry_level_and_no_identified_level', 'other_education_destinations'
     for i in ['fe_level_3', 'fe_level_2', 'fe_entry_level_and_no_identified_level', 'other_education_destinations']:
         filtered_data = filtering(
             data, facts=i, dat='2020/21', subfilts=['characteristic_group', 'student_characteristic'])
@@ -27,6 +26,15 @@ if __name__ == '__main__':
         
         # write to file
         filtered_data.to_csv(path)
+
+    # for i in ['fe_level_3']:
+    #     filtered_SEN_data = filtering(
+    #         SEN_data, facts=i, dat='2020/21', subfilts=['characteristic_group', 'student_characteristic'])
+    #         filtered_SEN_data = filtered_SEN_data.loc[["LLDD Provision", "SEN Provision"]].drop(columns=['Disadvantaged', 'Female', 'Male', 'Not Disadvantaged'])
+    #@TODO find way to merge with above to avoid wriitng to separate file.
+    
+
+
     #@TODO add code below to get visualisations for people going on to apprenticeships at levels 2, 3, 4. 
     # for i in ['higher_and_degree_apprenticeships_level_4_and_above', 'advanced_apprenticeships_level_3', 'intermediate_apprenticeships_level_2']:
     #     filtered_data = filtering(
