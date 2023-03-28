f4a438ef56d6e3e5e8b12a2d3cec5293304cfbcd
Giles Dring <giles@dringtech.com>
2023-03-28 14:58:08 +0100

    Fixing links

--- site/_data/nav_menu.ts
+++ site/_data/nav_menu.ts
@@ -3,7 +3,7 @@ export default [
     name: "Supply",
     children: [
       { name: "Further Education", link: "/supply/fe" },
-      { name: "16-18 Destinations", link: "/supply/fe_dest" },
+      { name: "16-18 Destinations", link: "/supply/fe-dest" },
       { name: "Key Stage 4", link: "/supply/ks4" },
       { name: "A and AS Level", link: "/supply/a-level" },
     ],
