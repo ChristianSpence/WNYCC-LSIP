f3e4b8d1937f3000929478eab966e6fb0cba81f8
luke-strange <92686634+luke-strange@users.noreply.github.com>
2023-04-06 12:07:57 +0100

    Add NOMIS page

--- site/_data/nav_menu.ts
+++ site/_data/nav_menu.ts
@@ -2,15 +2,18 @@ export default [
   {
     name: "Supply",
     children: [
-      { name: "Further Education", link: "/supply/fe" },
-      { name: "16-18 Destinations", link: "/supply/fe-dest" },
-      { name: "Apprenticeships", link: "/supply/apprenticeships" },
-      { name: "Key Stage 4", link: "/supply/ks4" },
-      { name: "A and AS Level", link: "/supply/a-level" },
+      { name: "Further Education", link: "/supply/fe/" },
+      { name: "16-18 Destinations", link: "/supply/fe-dest/" },
+      { name: "Apprenticeships", link: "/supply/apprenticeships/" },
+      { name: "Key Stage 4", link: "/supply/ks4/" },
+      { name: "A and AS Level", link: "/supply/a-level/" },
     ],
   },
   {
     name: "Demand",
-    link: "/demand",
+    children: [
+      { name: "Postings", link: "/demand/postings/" },
+      { name: "NOMIS", link: "/demand/nomis/" },
+    ],
   },
 ];
